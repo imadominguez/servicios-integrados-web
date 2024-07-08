459291605bebe01f2d30abbf0be93180f40b0772
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-07-08 02:08:56 -0300

    feat: Add login, logout, and register actions for authentication

---
+++ src/actions/auth/login.ts
@@ -0,0 +1,40 @@
+'use server';
+
+import { signIn } from '@/auth.config';
+import { sleep } from '@/utils';
+import { AuthError } from 'next-auth';
+
+// ...
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn('credentials', {
+      ...Object.fromEntries(formData),
+      redirect: false,
+    });
+    return 'Success';
+  } catch (error) {
+    if ((error as AuthError).type === 'CredentialsSignin') {
+      return 'Credenciales incorrectas';
+    }
+
+    return 'Error inesperado';
+  }
+}
+
+export const login = async (email: string, password: string) => {
+  try {
+    await signIn('credentials', { email, password });
+
+    return { ok: true };
+  } catch (error) {
+    console.log(error);
+    return {
+      ok: false,
+      message: 'No se pudo iniciar sesión',
+    };
+  }
+};
