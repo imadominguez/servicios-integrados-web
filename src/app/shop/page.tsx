ebc2ab9dc027491a832f0c918c608bcc6d974cdf
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-07-10 05:37:18 -0300

    refactor: Improve loading component in shop page

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,11 +1,12 @@
 import { Products } from '@/components/products/products';
+import { sleep } from '@/utils';
 
 export type PageShopProps = {
   params: { [key: string]: string | string[] | undefined };
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
-export default function ShopPage(props: PageShopProps) {
+export default async function ShopPage(props: PageShopProps) {
   return (
     <div>
       <main className="max-w-[2000px] mx-auto px-3 xl:p-0">
