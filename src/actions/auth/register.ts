432dad134f41398377f7ce5b237c9c873e314761
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-07-10 02:01:10 -0300

    chore: documentacion sobre la funcionalidad de logueo y registro de usuario

--- src/actions/auth/register.ts
+++ src/actions/auth/register.ts
@@ -3,12 +3,16 @@
 import prisma from '@/lib/prisma';
 import bcryptjs from 'bcryptjs';
 
+// ?? Esta funcion debe registrar un usuario en la base de datos y ademas enviar un mail de confirmacion
+// ?? al usuario para que pueda confirmar su correo electronico
+
 export const registerUser = async (
   name: string,
   email: string,
-  password: string,
+  password: string
 ) => {
   try {
+    // ?? Se crea un usuario en la base de datos
     const user = await prisma.user.create({
       data: {
         name: name,
@@ -22,12 +26,17 @@ export const registerUser = async (
       },
     });
 
+    // ?? Se envia un mail de confirmacion al usuario
+    // user &&
+
+    // ?? Se retorna un objeto con la informacion del usuario creado y un mensaje de exito
     return {
       ok: true,
       user: user,
       message: 'Usuario creado',
     };
   } catch (error) {
+    // ?? En caso de error se imprime el error en consola y se retorna un objeto con un mensaje de error
     console.log(error);
 
     return {
