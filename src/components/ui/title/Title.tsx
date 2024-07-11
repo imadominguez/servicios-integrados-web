a18af0782ba8590ab19a820f9cbbbacd12c5dc14
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-07-10 21:14:48 -0300

    feat: Add Title component for displaying titles and subtitles

---
+++ src/components/ui/title/Title.tsx
@@ -0,0 +1,25 @@
+import { font } from '@/config/fonts';
+
+// Definición de la interface de las Props del componente
+interface Props {
+  title: string;
+  subtitle?: string;
+  className?: string;
+}
+
+export const Title = ({ title, subtitle, className }: Props) => {
+  return (
+    // Contenedor principal del título con posibilidad de aplicar una clase personalizada
+    <div className={`${className ? className : ''} my-3`}>
+      {/* Título principal con estilos de fuente específicos */}
+      <h1
+        className={`${font.className} mb-3 text-2xl font-semibold antialiased md:text-2xl`}
+      >
+        {title}
+      </h1>
+
+      {/* Subtítulo opcional con estilos de fuente específicos */}
+      {subtitle && <h3 className="mb-5 text-base md:text-xl">{subtitle}</h3>}
+    </div>
+  );
+};
