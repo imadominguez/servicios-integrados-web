2c6e9b5487dc792115bc1b99f3dadd07aa2a9642
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-07-10 21:36:19 -0300

    refactor: Add fade-up animation to page elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,19 +9,31 @@ export default function HomePage() {
       <div className="max-w-[2000px] mx-auto">
         <Carrousel />
         <div className="grid md:grid-cols-3 p-20 py-12 px-5 bg-muted-foreground/5 gap-10">
-          <div className="grid place-content-center text-center">
+          <div
+            data-aos="fade-up"
+            data-aos-duration="500"
+            className="grid place-content-center text-center"
+          >
             <h2 className=" font-bold">Envíos</h2>
             <p className="opacity-90 text-sm">
               Realizamos envíos a todo el país.
             </p>
           </div>
-          <div className="grid place-content-center text-center">
+          <div
+            data-aos="fade-up"
+            data-aos-duration="900"
+            className="grid place-content-center text-center"
+          >
             <h2 className=" font-bold">Métodos de envío</h2>
             <p className="opacity-90 text-sm">
               Envíos a través de Correo Argentino.
             </p>
           </div>
-          <div className="grid place-content-center text-center">
+          <div
+            data-aos="fade-up"
+            data-aos-duration="500"
+            className="grid place-content-center text-center"
+          >
             <h2 className=" font-bold">Métodos de pago</h2>
             <p className="opacity-90 text-sm">Podés pagar en hasta 12 cuotas</p>
           </div>
--- src/components/pages/home/about.tsx
+++ src/components/pages/home/about.tsx
@@ -3,11 +3,19 @@ import React from 'react';
 export const About = () => {
   return (
     <div className="p-10 grid place-content-center text-center py-20 pb-10">
-      <h3 className="text-xl md:text-3xl lg:text-4xl xl:text-4xl text-center font-semibold text-primary mb-10">
+      <h3
+        data-aos="fade-up"
+        data-aos-duration="500"
+        className="text-xl md:text-3xl lg:text-4xl xl:text-4xl text-center font-semibold text-primary mb-10"
+      >
         Sobre Nosotros
       </h3>
 
-      <p className="mb-20 my-5 max-w-4xl leading-7 tracking-normal">
+      <p
+        data-aos="fade-up"
+        data-aos-duration="900"
+        className="mb-20 my-5 max-w-4xl leading-7 tracking-normal"
+      >
         Descubre nuestra plataforma integral que ofrece servicios expertos en
         calefacción, seguridad y computación. Desde instalaciones de sistemas de
         calefacción eficientes hasta soluciones de seguridad avanzadas y
